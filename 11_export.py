ef5a5730c967e6559c5951af7c9a936dba7f4689
chauhana1013 <100536433+chauhana1013@users.noreply.github.com>
2024-02-21 09:18:29 +1300

    Working 'write to file'.

---
+++ 11_export.py
@@ -0,0 +1,75 @@
+import pandas
+import random
+from datetime import date
+
+# Currency Formatting Function
+def currency(x):
+    return f"${x:.2f}"
+
+# Dictionaries to hold ticket details
+all_names = ["a", "b", "c", "d", "e"]
+all_ticket_costs = [7.50, 7.50, 10.50, 10.50, 6.50]
+surcharge = [0, 0, 0.53, 0.53, 0]
+
+mini_movie_dict = {
+    "Name": all_names,
+    "Ticket Price": all_ticket_costs,
+    "Surcharge": surcharge
+}
+mini_movie_frame = pandas.DataFrame(mini_movie_dict)
+
+# Calculate the total cost (ticket + surcharge)
+mini_movie_frame['Total'] = mini_movie_frame['Surcharge'] + mini_movie_frame['Ticket Price']
+
+# Calculate the profit for each ticket
+mini_movie_frame['Profit'] = mini_movie_frame['Ticket Price'] - 5
+
+# Calculate ticket and profit totals
+total = mini_movie_frame['Total'].sum()
+profit = mini_movie_frame['Profit'].sum()
+
+# Choose winner and look up total won
+winner_name = random.choice(all_names)
+win_index = all_names.index(winner_name)
+total_won = mini_movie_frame.at[win_index, 'Total']
+
+# Set index at end (before printing)
+mini_movie_frame = mini_movie_frame.set_index('Name')
+
+# **** Get current date for heading and file name ****
+# Get today's date
+today = date.today()
+
+# Get day, month and year as individual strings
+day = today.strftime("%d")
+month = today.strftime("%m")
+year = today.strftime("%Y")
+
+heading = f"---- Mini Movie Fundraiser Ticket Data ({day}/{month}/{year}) ----\n"
+filename = f"MMF_{year}_{month}_{day}"
+
+# Change frame to a string so that we can export it to file
+mini_movie_string = pandas.DataFrame.to_string(mini_movie_frame)
+
+# Create strings for printing
+ticket_cost_heading = "\n----- Ticket Cost / Profit -----"
+total_ticket_sales = f"Total Ticket Sales: ${total}"
+total_profit = f"Total Profit: ${profit}"
+
+# Edit text below!! I
+sales_status = "\n*** All the tickets have been sold ***"
+
+winner_heading = "\n---- Raffle Winner ----"
+winner_text = 
+
+print(mini_movie_frame)
+
+print()
+print("*---- Raffle Winner ----*")
+print(f"Congratulations {winner_name}. You have won ${total_won} ie: your ticket is free")
+
+
+
+
+
+
